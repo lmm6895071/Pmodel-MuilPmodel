b4455b467caae645dfb5bce8865f3134565dddd9
lmm6895071 <lmm6895071@126.com>
2017-07-05 20:23:16 +0800

    update

--- myssh/Rssh.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-date 
-
-ssh -fCNR 10101:localhost:22 root@139.199.119.82
-#autossh -M 1221 -f -N -R 22345:localhost:22 root@139.199.119.82
-
--- myssh/assh.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-comd="`ps aux | grep 139.199.119.82 | grep -v grep`"
-if test -z "$comd" 
-then
-    ssh -fCNR 13345:localhost:22 root@139.199.119.82
-fi
-
